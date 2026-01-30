32979800f5068a049049fab70a4f1bdaf7c83642
Shubham Malhotra <shubhammalhotra9494@gmail.com>
2026-01-30 12:10:47 +0530

    Add files via upload

---
+++ boygirl.py
@@ -0,0 +1,8 @@
+x = input()
+
+y = len(x)
+
+if y % 2 == 0:
+    print("IGNORE HIM!")
+else:
+    print("CHAT WITH HER!")    
---
+++ petyastring2.py
@@ -0,0 +1,7 @@
+a = input("Enter string1:")
+b = input("Emter string2:")
+
+x = len(a)
+y = len(b)
+
+print(x-y)
